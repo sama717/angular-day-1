756863cb0029b7c5c3976f02b3038c5c92fec13d
sama717 <samafarag7@gmail.com>
2026-04-26 13:32:22 +0300

    ecommerce angular task

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Slider } from './components/slider/slider';
+import { Products } from './components/products/products';
+import { Footer } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Slider, Products, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
